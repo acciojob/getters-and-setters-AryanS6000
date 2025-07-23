ab08422baa9aa52a99ece4beb33dddc2f0c1b415
AryanS6000 <163299006+AryanS6000@users.noreply.github.com>
2025-07-23 10:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
-//complete this code
 class Person {
-  #name;
-  #age;
-
   constructor(name, age) {
-    this.#name = name;
-    this.#age = age;
+    this._name = name;
+    this._age = age;
   }
 
   get name() {
-    return this.#name;
+    return this._name;
   }
 
   set name(newName) {
-    this.#name = newName;
+    this._name = newName;
   }
 
   get age() {
-    return this.#age;
+    return this._age;
   }
 
   set age(newAge) {
-    this.#age = newAge;
+    this._age = newAge;
   }
 }
 
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
+// Do not change the code below this
 
 // Do not change the code below this line
 window.Person = Person;
